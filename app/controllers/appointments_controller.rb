25f8240892fd83b5b95423cbac677a1000692b25
Michael <michaelforemail@protonmail.com>
2022-08-09 21:12:03 -0500

    appointment edit page

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,12 +1,17 @@
 class AppointmentsController < ApplicationController
+    protect_from_forgery except: :create
     def new
     end
     def create
-        app = Appointment.create()
-        puts "CREATED"
+        flash.notice = "Appointment Created"
+        start_time = params[:start_time]
+        end_time = params[:end_time]
+        app = Appointment.create(start_time: start_time, end_time: end_time)
         redirect_to root_path
     end
     def index
         @appointments = Appointment.all
+    end
+    def edit
     end
 end
