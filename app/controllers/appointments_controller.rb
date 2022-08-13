9f54b5c23cf7e43fc831fd78ecc8cf50bb003acb
Michael <michaelforemail@protonmail.com>
2022-08-13 00:04:10 -0500

    added appointment edit

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -12,6 +12,14 @@ class AppointmentsController < ApplicationController
     def index
         @appointments = Appointment.all
     end
+    def update
+        app = Appointment.find(params[:id])
+        time = DateTime.new(params[:appointment]["start_time(1i)"].to_i, params[:appointment]["start_time(2i)"].to_i, params[:appointment]["start_time(3i)"].to_i, params[:appointment]["start_time(4i)"].to_i, params[:appointment]["start_time(5i)"].to_i)
+        app.update(start_time: time)
+        app.update(end_time: time + 30.minutes)
+        redirect_to root_path
+    end
     def edit
+        @appointment = Appointment.find(params[:id])
     end
 end
